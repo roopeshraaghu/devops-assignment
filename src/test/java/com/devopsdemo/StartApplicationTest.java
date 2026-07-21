8cb31983daecf9754ef4e8a9e2af61562ae9e787
roopesh <roopesh.p@nagarro.com>
2026-07-21 13:43:26 +0530

    test cases

---
+++ src/test/java/com/devopsdemo/StartApplicationTest.java
@@ -0,0 +1,25 @@
+package com.devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import org.springframework.ui.ConcurrentModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartApplicationTest {
+
+    @Test
+    void testIndex() {
+
+        StartApplication app = new StartApplication();
+        Model model = new ConcurrentModel();
+
+        String viewName = app.index(model);
+
+        assertEquals("index", viewName);
+        assertEquals(
+                "Git hub actions Deployed!!!! - Modified_Text",
+                model.getAttribute("msg")
+        );
+    }
+}
